c93f1b14d318736b24f2a6df802057d2a11bf71a
bbist1 <bishnu.bista1@aexp.com>
2023-08-31 11:05:10 -0700

    fix(test): refactored

--- src/util/index.js
+++ src/util/index.js
@@ -1,68 +1,77 @@
-import path from 'path';
-import fs from 'fs';
-/* eslint-disable no-console -- console used in tests */
-const chalk = require('chalk');
+/*
+ * Copyright 2023 American Express Travel Related Services Company, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express
+ * or implied. See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
 
+import path from 'path';
+import fs, { promises as fsPromises } from 'fs';
+import chalk from 'chalk';
+/* eslint-disable no-console -- console used in tests */
 export const getUserHomeDirectory = () => process.env.HOME || process.env.USERPROFILE;
 export const cacheFileName = '.one-app-module-cache';
+export const moduleCacheFileName = '.one-app-module-map-cache';
 export const oneAppDirectoryName = '.one-app';
 export const oneAppDirectoryPath = path.join(getUserHomeDirectory(), oneAppDirectoryName);
 export const oneAppModuleCachePath = path.join(oneAppDirectoryPath, cacheFileName);
 
 // show cache size and how to delete info on start
-export const showCacheInfo = () => {
-  fs.stat(oneAppModuleCachePath, (error, stats) => {
-    if (error) {
-      console.error('There was error checking file stat', error);
-    } else {
-      const fileSizeOnMB = stats.size / (1024 * 1024); // bytes to mb
-      const message = `File size of ${cacheFileName}: ${chalk.bold.greenBright(fileSizeOnMB.toFixed(2), 'MB')}`;
-      const separator = '*'.repeat(message.length);
-      console.log(chalk.bold.cyanBright(separator));
-      console.log(chalk.bold.redBright('CACHE INFORMATION'));
-      console.log(message);
-      console.log(`To delete cache, please run \n  ${chalk.bold.redBright('  rm ', oneAppModuleCachePath)}`);
-      console.log(chalk.bold.cyanBright(separator));
-    }
-  });
+export const showCacheInfo = async () => {
+  try {
+    const stats = await fsPromises.stat(oneAppModuleCachePath);
+    const fileSizeOnMB = stats.size / (1024 * 1024); // bytes to mb
+    const message = `File size of ${cacheFileName}: ${chalk.bold.greenBright(fileSizeOnMB.toFixed(2), 'MB')}`;
+    const separator = '*'.repeat(message.length);
+    console.log(chalk.bold.cyanBright(separator));
+    console.log(chalk.bold.redBright('CACHE INFORMATION'));
+    console.log(message);
+    console.log(`To delete cache, please run \n  ${chalk.bold.redBright('  rm ', oneAppModuleCachePath)}`);
+    console.log(chalk.bold.cyanBright(separator));
+  } catch (error) {
+    console.error('There was error checking file stat', error);
+  }
 };
 
 // setup folder and file
-export const setupCacheFile = () => {
-  fs.mkdir(oneAppDirectoryPath, { recursive: true }, (error) => {
-    if (error) {
-      console.error(`There was error creating ${oneAppDirectoryName} directory`);
-    } else {
-      console.log(`Successfully created ${oneAppDirectoryPath}`);
-      console.log(`Creating ${cacheFileName}`);
-      try {
-        fs.writeFileSync(oneAppModuleCachePath, JSON.stringify('{}'));
-        console.log(`${cacheFileName} created successfully on ${oneAppModuleCachePath}`);
-      } catch (err) {
-        console.error(`Error creating ${cacheFileName} on ${oneAppModuleCachePath}, \n${err}`);
-      }
+export const setupCacheFile = async () => {
+  try {
+    await fsPromises.mkdir(oneAppDirectoryPath, { recursive: true });
+    console.log(`Successfully created ${oneAppDirectoryPath}`);
+    console.log(`Creating ${cacheFileName}`);
+    try {
+      fs.writeFileSync(oneAppModuleCachePath, JSON.stringify('{}'));
+      console.log(`${cacheFileName} created successfully on ${oneAppModuleCachePath}`);
+    } catch (err) {
+      console.error(`Error creating ${cacheFileName} on ${oneAppModuleCachePath}, \n${err}`);
     }
-  });
+  } catch (error) {
+    console.error(`There was error creating ${oneAppDirectoryName} directory`);
+  }
 };
 
 // gets cached module from ~/.one-app/.one-app-module-cache
-export const getCachedModules = () => {
-  let hasCachedFile = false;
-  if (fs.existsSync(oneAppModuleCachePath)) {
-    hasCachedFile = true;
-  } else {
-    setupCacheFile();
+export const getCachedModules = async () => {
+  if (!fs.existsSync(oneAppModuleCachePath)) {
+    await setupCacheFile();
+    return {};
   }
-  if (hasCachedFile) {
-    try {
-      showCacheInfo();
-      const cachedContent = fs.readFileSync(oneAppModuleCachePath, 'utf8');
-      return JSON.parse(cachedContent);
-    } catch (err) {
-      console.error('Could not parse JSON content', err);
-    }
+
+  try {
+    await showCacheInfo();
+    const cachedContent = fs.readFileSync(oneAppModuleCachePath, 'utf8');
+    return JSON.parse(cachedContent);
+  } catch (err) {
+    console.error('Could not parse JSON content', err);
+    return {};
   }
-  return {};
 };
 
 let timerId = null;
@@ -92,5 +101,4 @@ export const removeDuplicatedModules = (url, cachedModules, moduleNames) => {
   });
   return updatedCachedModules;
 };
-
 /* eslint-enable no-console -- because eslint-comments/disable-enable-pair */
